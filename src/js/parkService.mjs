733cc1b14eaf36be81b24baf5c2520bf9248905a
NateTheUnavailable <nate@stevenogardner.com>
2025-02-06 11:27:50 -0700

    working NPS 4

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -183,19 +183,20 @@ const parkInfoLinks = [
     name: "Current Conditions &#x203A;",
     link: "conditions.html",
     image: park.images[2].url,
-    description: "See what conditions to expect in the park before leaving on your trip!",
+    description:
+      "See what conditions to expect in the park before leaving on your trip!"
   },
   {
     name: "Fees and Passes &#x203A;",
     link: "fees.html",
     image: park.images[3].url,
-    description: "Learn about the fees and passes that are available.",
+    description: "Learn about the fees and passes that are available."
   },
   {
     name: "Visitor Centers &#x203A;",
     link: "visitor_centers.html",
     image: park.images[9].url,
-    description: "Learn about the visitor centers in the park.",
+    description: "Learn about the visitor centers in the park."
   }
 ];
 
@@ -218,15 +219,24 @@ async function getJson(url) {
 }
 
 export function getInfoLinks(data) {
-  // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
   const withUpdatedImages = parkInfoLinks.map((item, index) => {
-    item.image = data[index + 3].url;
+    item.image = data[index + 2].url;
     return item;
   });
-return withUpdatedImages;
+  return withUpdatedImages;
 }
 
 export async function getParkData() {
-  const parkData = await getJson("parks?parkCode=yell");
+  const parkData = await getJson("parks?parkCode=yell ");
   return parkData.data[0];
+}
+
+export async function getParkAlerts(code) {
+  const parkData = await getJson(`alerts?parkCode=${code}`);
+  return parkData.data;
+}
+
+export async function getParkVisitorCenters(code) {
+  const parkData = await getJson(`visitorcenters?parkCode=${code}`);
+  return parkData.data;
 }
